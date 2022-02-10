0695c816873fd6c1bc1beed98612799786a33feb
Josh Foster <joshfost@gmail.com>
2022-02-10 01:33:53 -0600

    added bottom card for mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottomSheetSelector } from './app.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomSheetSelector
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     BrowserAnimationsModule,
     MatButtonToggleModule,
-    MatButtonModule
+    MatButtonModule,
+    MatBottomSheetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
